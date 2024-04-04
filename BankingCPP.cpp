3e53bb20565a4fd7b4d6c21924ab5b0058bca115
makelorig <makelorig>
2024-04-04 13:12:16 +0300

    Added delClient, foundByID

--- BankingCPP.cpp
+++ BankingCPP.cpp
@@ -3,6 +3,8 @@
 #define random(min,max) min+rand()%(max-min+1)
 using namespace std;
 
+class Bank;
+
 class Client {
 	friend class Bank;
 private:
@@ -32,11 +34,27 @@ public:
 	void setSecondName(string secondName) {
 		this->secondName = secondName;
 	}
+	void setAge(unsigned int age) {
+		this->age = age;
+	}
+	void setBalance() {
+		 this->balance = balance;
+	}
 	string getFirstName() {
 		return this->firstName;
 	}
-
+	string getSecondName() {
+		return this->secondName;
+	}
+	unsigned int getAge() {
+		return this->age;
+	}
+	double getBalance() {
+		return this->balance;
+	}
+	
 	void clientInfo() {
+		cout << endl;
 		cout << "First Name: " << this->firstName << endl;
 		cout << "Second Name: " << this->secondName << endl;
 		cout << "Balance: " << this->balance << endl;
@@ -45,19 +63,18 @@ public:
 	}
 
 };
-unsigned int Client::lastId = 0;
+unsigned int Client::lastId = 1;
 
 class Bank
 {
 private:
-	string bankName;
-	Client** clients = new Client * [10];
+	string name;
+	Client* clients = new Client[10];
 	unsigned int size = 0;
 public:
-	Bank() {
-		this->bankName = bankName;
+	Bank(string name) {
+		this->name = name;
 	}
-	/*int operator = (Bank);*/
 	void changeBalance(Client bankAccaunt, double value) {
 		char f;
 		cin >> f;
@@ -72,68 +89,89 @@ public:
 		}
 	}
 
-	~Bank() {
-		for (int i = 0; i < size; i++)
-		{
-			delete clients[i];
-		}
-		delete[] this->clients;
-	}
 
-
-	void createClient() {
-		Client** buf = new Client * [size + 1];
+	void createClient(string firstName, string secondName, unsigned short age) {
+		Client* buf = new Client[size + 1];
 		for (int i = 0; i < size; i++)
 		{
 			buf[i] = clients[i];
 		}
 		delete[]  clients;
 		clients = buf;
-		size++;
+		clients[size++] = Client(firstName, secondName, age, createBA());
 	}
-	//void deleteClient() {
-	//	if (size == 0) {
-	//		cout << "Clients aren't avalible";
-	//	}
 
-	//}
-
-	long long createBA() {
-		int uid = random(1000000000000000, 9999999999999999);
+	unsigned long long createBA() {
+		unsigned long long uid = 0;
 		bool flag = false;
+		do {
+			uid += random(1000, 9999) * 10000;
+			uid += random(1000, 9999);
+		} while (!checkBankAcccount(uid));
 
+		return uid;
+	}
+	bool checkBankAcccount(unsigned long long bankAccount) {
 		for (int i = 0; i < size; i++)
 		{
-			if (clients[i]->bankAccount == uid) {
-
-			}
+			if (clients[i].bankAccount == bankAccount)return false;
 		}
-
-
+		return true;
 	}
-	void BankInfo() {
-		cout << "Name: " << this->bankName << endl;
+	void bankInfo() {
+		cout << "Name: " << this->name << endl;
 		cout << "Cars count: " << this->size << endl;
 		if (size == 0)cout << "Bank is empty" << endl;
 		else {
 			cout << "Clients: " << endl;
 			for (int i = 0; i < size; i++)
 			{
-				clients[i]->clientInfo();
+				clients[i].clientInfo();
 			}
 
 		}
-
+	}
+	void showClients() {
+		for (int i = 0; i < size; i++)
+		{
+			clients[i].clientInfo();
+		}
+	}
+	~Bank() {
+		delete[] clients;
 	}
 
+	void delClient(int id) {
+		Client* buf = new Client[size - 1];
+
+		for (int i = 0,k=0; i < size; i++)
+		{
+			if(this->clients[i].id!=id) buf[k++] = clients[i];
+		}
+		delete[]  clients;
+		clients = buf;
+	}
+
+	void foundByID(int id) {
+		if (id > size || id <= 0) {
+			cout << "ID is difernt";
+		}
+		else {
+			clients[id-1].clientInfo();
+		}
+	}
 };
 
 int main()
 {
 	srand(time(NULL));
-	/*Client** clients = new Client * [10];
-	for (int i = 0; i < 100; i++)
+	Bank bk1("Hitbank");
+	for (int i = 0; i < 20; i++)
 	{
-		clients[i] = new clients[i]();
-	}*/
+		bk1.createClient("Nigr", "Nillers", 52);
+	}
+	bk1.bankInfo();
+	bk1.delClient(14);
+	bk1.foundByID(15);
+	bk1.bankInfo();
 }
